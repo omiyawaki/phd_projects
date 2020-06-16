a8efee35cbcad63198ff230b042bd60a9c18c996
Osamu Miyawaki <omiyawaki@Osamus-MacBook-Pro.local>
2020-06-16 11:42:36 -0500

    added w02 notes

---
+++ 002/notes/20200617/.auctex-auto/20200617_002_w02.el
@@ -0,0 +1,48 @@
+(TeX-add-style-hook
+ "20200617_002_w02"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:org8d9cec4"
+    "sec:orge82857e"
+    "sec:orgdb3cdf1"
+    "sec:orgafb3a6e"
+    "sec:orgc0c41cd"
+    "sec:org066fc93"
+    "fig:orgd2d127f"
+    "fig:org8752234"
+    "fig:org73c353a"
+    "sec:orge358300"
+    "sec:orgbe24baa")
+   (LaTeX-add-bibliographies
+    "../../../../../Sync/Papers/references"))
+ :latex)
+
