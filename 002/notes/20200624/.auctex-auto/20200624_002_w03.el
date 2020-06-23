29adf55b5e34e74889f79c6b380d5259b012c87c
Osamu Miyawaki <me@omiyawaki.com>
2020-06-23 16:15:51 -0500

    20200624 notes checkpoint 1

--- 002/notes/20200624/.auctex-auto/20200624_002_w03.el
+++ 002/notes/20200624/.auctex-auto/20200624_002_w03.el
@@ -5,13 +5,13 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,17 +31,23 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:orga40ad3e"
-    "sec:org84e4e3f"
-    "sec:org2613404"
-    "sec:orgff29c17"
-    "sec:orgb38297f"
-    "sec:org8814364"
-    "sec:org4f7595a"
-    "fig:org9bca2ab"
-    "fig:orgf05a71d"
-    "fig:org8d3d374"
-    "sec:orgfc70353"
-    "sec:org8b64c90"))
+    "sec:orgbce2332"
+    "sec:orgdc9e3cc"
+    "sec:org58bd427"
+    "sec:orgc9cefe0"
+    "sec:org7795033"
+    "sec:org2efab71"
+    "sec:orge5788bd"
+    "fig:org7a8841e"
+    "fig:org5d1a2c0"
+    "fig:org343fee3"
+    "sec:org7f5e928"
+    "fig:org652d7a0"
+    "fig:org1b9c61d"
+    "fig:orgf6fa329"
+    "fig:orge273189"
+    "sec:org382cad9")
+   (LaTeX-add-bibliographies
+    "../../../../../Sync/papers/references"))
  :latex)
 
