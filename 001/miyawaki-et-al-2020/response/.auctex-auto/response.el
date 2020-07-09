f24aa114ad9861f5c931d26ae3e43628d981d2ec
Osamu Miyawaki <me@omiyawaki.com>
2020-07-08 19:20:37 -0500

    completed first pass of reviewer 1

---
+++ 001/miyawaki-et-al-2020/response/.auctex-auto/response.el
@@ -0,0 +1,41 @@
+(TeX-add-style-hook
+ "response"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "margin=1in") ("parskip" "parfill")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "geometry"
+    "parskip")
+   (LaTeX-add-labels
+    "sec:org70fe662"
+    "sec:orgf0edd62")
+   (LaTeX-add-bibliographies
+    "../../../../../../mnt/c/Users/omiyawaki/Sync/papers/references"))
+ :latex)
+
