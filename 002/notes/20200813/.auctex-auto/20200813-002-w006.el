42a161f45b0fe34280f11d89d000ee620f98f752
Osamu Miyawaki <me@omiyawaki.com>
2020-09-12 09:49:23 -0500

    resume project on 20200912

--- 002/notes/20200813/.auctex-auto/20200813-002-w006.el
+++ 002/notes/20200813/.auctex-auto/20200813-002-w006.el
@@ -1,5 +1,5 @@
 (TeX-add-style-hook
- "20200806-002_w005"
+ "20200813-002-w006"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "11pt")))
@@ -31,22 +31,42 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:org75b15d6"
-    "sec:org7fea45e"
-    "fig:org865427b"
-    "fig:orgdc967fb"
-    "fig:orgc342aab"
-    "fig:org91c292c"
-    "fig:orgc52acad"
-    "fig:org2c059cb"
-    "sec:org6ccb884"
-    "fig:org5bcf633"
-    "fig:org99dcb89"
-    "fig:orgcf86695"
-    "sec:orga4e0468"
-    "fig:org0dd4041"
-    "fig:orgf832265"
-    "sec:org67bc814")
+    "sec:orgf4b5fd4"
+    "sec:org202f2f8"
+    "fig:org6bb4767"
+    "sec:org54f98fe"
+    "sec:orgf1e72ac"
+    "sec:orgc858e38"
+    "fig:org43fc374"
+    "fig:org1fa1e68"
+    "sec:org5643330"
+    "sec:org61de612"
+    "sec:org4e21439"
+    "fig:orga08856b"
+    "fig:org17106c3"
+    "sec:orgece31ed"
+    "sec:orgbbb8d7d"
+    "sec:org817b123"
+    "fig:org845a067"
+    "fig:orgdb46e7a"
+    "fig:org5617d48"
+    "sec:org8bd0b2a"
+    "sec:org5fe1813"
+    "sec:org0a06548"
+    "fig:org7193867"
+    "fig:org075c5db"
+    "fig:org80bb966"
+    "sec:orgf9e0a67"
+    "fig:org323adda"
+    "sec:org172d899"
+    "fig:org41a190c"
+    "fig:org904dc58"
+    "fig:org8cb1bab"
+    "fig:orgf9db9e1"
+    "fig:org0e7e926"
+    "fig:org18c58bc"
+    "fig:orga993cae"
+    "sec:org65d6f89")
    (LaTeX-add-bibliographies
     "../../../../../../mnt/c/Users/omiyawaki/Sync/papers/references"))
  :latex)
